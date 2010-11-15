c15fda94c9eeada9a39f17289e7269b76a093104
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-11-15 15:20:54 -0500

    Going back to a single URL.

--- chutney_server/chutney/urls.py
+++ chutney_server/chutney/urls.py
@@ -3,8 +3,7 @@ from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
 urlpatterns = patterns('chutney.views',
-    url(r'^chutney.js$', 'assemble_js', name='chutney.js'),
-    url(r'^chutney_debug.js$', 'assemble_js', {'debug': True}, name='chutney_debug.js'),
+    url(r'^chutney.js$', 'assemble_js', {'debug': True}, name='chutney.js'),
     url(r'^org_info.json', 'org_info', name='chutney.org_info.json'),
     url(r'^names.json', 'name_search', name='chutney.names.json'),
     url(r'^debug/$', 'debug_create', name='chutney.debug_create'),
